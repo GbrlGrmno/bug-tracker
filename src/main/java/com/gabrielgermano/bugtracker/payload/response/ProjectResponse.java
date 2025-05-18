2b1d638e2da3a039e872271a95b87cd22acf9819
GbrlGrmno <gabri1020m@gmail.com>
2025-05-18 10:35:39 -0300

    feat: Add audit attributes to ProjectResponse

--- src/main/java/com/gabrielgermano/bugtracker/payload/response/ProjectResponse.java
+++ src/main/java/com/gabrielgermano/bugtracker/payload/response/ProjectResponse.java
@@ -1,15 +1,26 @@
 package com.gabrielgermano.bugtracker.payload.response;
 
+import java.time.LocalDateTime;
+
 public class ProjectResponse {
 
     private Long id;
     private String name;
     private String description;
 
-    public ProjectResponse(Long id, String name, String description) {
+    private String createdBy;
+    private LocalDateTime createdDate;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+    public ProjectResponse(Long id, String name, String description, String createdBy, LocalDateTime createdDate, String lastModifiedBy, LocalDateTime lastModifiedDate) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+        this.lastModifiedBy = lastModifiedBy;
+        this.lastModifiedDate = lastModifiedDate;
     }
 
     public ProjectResponse() {}
@@ -37,4 +48,37 @@ public class ProjectResponse {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
 }
